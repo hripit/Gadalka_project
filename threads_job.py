02dae8cf574ce4290a48c9dd508ef6a08cfa4040
HriPit <67256710+hripit@users.noreply.github.com>
2025-01-14 03:17:23 +0500

    Revert "Initial commit"
    This reverts commit c1471ade

--- threads_job.py
+++ threads_job.py
@@ -15,15 +15,13 @@ def check_available_data(params: dict):
         data = pd.DataFrame(data=data, columns=['date_time'], dtype='datetime64[ns]', index=['date_time'])
         data['date_time'] = pd.to_datetime(data['date_time'])
         data['date_time_concat'] = data['date_time']
-        # data['date_time'] = pd.to_datetime(data.astype('int32').dtypes)
 
         pd_data_range = pd.date_range(start=args[2][0], end=args[2][1], freq='min', normalize=True)
         pd_data_range = pd.DataFrame(data=pd_data_range, columns=['date_time'])
+        empty = pd_data_range.merge(data, on='date_time', how='left')
+        empty = empty[empty['date_time_concat'].isnull()]
 
-        pd_date_range_empty = pd_data_range.merge(data, on='date_time', how='left')
-
-        # pd_date_range_empty = pd_data_range.merge(data, how='left', left_on='date_time', right_on='date_time')
-
+        params[args[0]][args[1]]['job_times'] = empty
 
     for key_schema in params.keys():
         for key_symbol in params[key_schema].keys():
