00c8a8271a24b2bae3e4383c1a521c3da34d8d48
HriPit <67256710+hripit@users.noreply.github.com>
2025-01-14 16:36:39 +0500

    Revert "Initial commit"
    This reverts commit c1471ade

--- binance_job.py
+++ binance_job.py
@@ -1,10 +1,11 @@
 from binance.spot import Spot
 from binance.error import ClientError
+from parameters import PARAMS_BI
 
 
 def get_connection_binance():
     try:
-        connection = Spot(PARAMS_API["api_key"], PARAMS_API["api_secret"])
+        connection = Spot(PARAMS_BI["api_key"], PARAMS_BI["api_secret"])
     except ClientError as error:
         connection = False
 
--- parameters.py
+++ parameters.py
@@ -4,3 +4,5 @@ PARAMS_DB = {
     "host": "127.0.0.1",
     "port": "5432",
     "dbname": "BTC"}
+
+PARAMS_BI = {"api_key": None, 'api_secret': None}
